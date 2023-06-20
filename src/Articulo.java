b24066b1621e8585383acba4abf6a38c96373a07
Neutrovertido <russellcode.james@gmail.com>
2023-06-20 15:28:36 -0600

    Parent Class Articulo

---
+++ src/Articulo.java
@@ -0,0 +1,62 @@
+public class Articulo {
+    String codigo;
+    double precio;
+    int disponible;
+    boolean estado;
+    String marca;
+
+    // Constructor
+    public Articulo(String codigo, double precio, int disponible, boolean estado, String marca) {
+        this.codigo = codigo;
+        this.precio = precio;
+        this.disponible = disponible;
+        this.estado = estado;
+        this.marca = marca;
+    }
+
+    // Getters
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public int getDisponible() {
+        return this.disponible;
+    }
+
+    public boolean getEstado() {
+        return this.estado;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    // Setters
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setDisponible(int disponible) {
+        this.disponible = disponible;
+    }
+
+    // Methods
+    public String getAtributos() {
+        String estado;
+        if (getEstado()) {
+            estado = "Nuevo";
+        } else {
+            estado = "Usado";
+        }
+
+        return "Código: " + getCodigo() +
+                "Precio: " + getPrecio() +
+                "Cantidad disponible: " + getDisponible() +
+                "Estado: " + estado +
+                "Marca: " + getMarca();
+    }
+}
