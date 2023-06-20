60786581f7f65dc459410221aa46ea6e1a17f137
Neutrovertido <russellcode.james@gmail.com>
2023-06-20 15:44:39 -0600

    Child Class Monitor

---
+++ src/Monitor.java
@@ -0,0 +1,34 @@
+public class Monitor extends Articulo{
+    String tecnologia;
+    String tamano;
+    String resolucion;
+
+    public Monitor(String codigo, double precio, int disponible, boolean estado, String marca, String tecnologia, String tamano, String resolucion) {
+        super(codigo, precio, disponible, estado, marca);
+        this.tecnologia = tecnologia;
+        this.tamano = tamano;
+        this.resolucion = resolucion;
+    }
+
+    // Getters
+    public String getTecnologia() {
+        return this.tecnologia;
+    }
+
+    public String getTamano() {
+        return this.tamano;
+    }
+
+    public String getResolucion() {
+        return this.resolucion;
+    }
+
+    // Methods
+    @Override
+    public String getAtributos() {
+        return super.getAtributos() +
+                "Tecnología de pantalla: " + getTecnologia() +
+                "Tamaño: " + getTamano() +
+                "Resolución: " + getResolucion();
+    }
+}
