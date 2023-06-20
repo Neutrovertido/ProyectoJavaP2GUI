a718d91daeafdeb889e1b977b67a22ff19c40f9a
Neutrovertido <russellcode.james@gmail.com>
2023-06-20 15:37:24 -0600

    Child Class PC

---
+++ src/PC.java
@@ -0,0 +1,27 @@
+public class PC extends Articulo{
+    String tipoPC;
+    String specs;
+
+    public PC(String codigo, double precio, int disponible, boolean estado, String marca, String tipoPC, String specs) {
+        super(codigo, precio, disponible, estado, marca);
+        this.tipoPC = tipoPC;
+        this.specs = specs;
+    }
+
+    // Getters
+    public String getTipoPC() {
+        return this.tipoPC;
+    }
+
+    public String getSpecs() {
+        return this.specs;
+    }
+
+    // Methods
+    @Override
+    public String getAtributos() {
+        return super.getAtributos() +
+                "Tipo: " + getTipoPC() +
+                "Especificaciones: " + getSpecs();
+    }
+}
