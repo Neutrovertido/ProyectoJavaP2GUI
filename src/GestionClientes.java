256982d2c92831dbb2e0d1d73c4c038e6e89550e
Neutrovertido <russellcode.james@gmail.com>
2023-06-22 14:32:42 -0600

    GestionClientes Form

---
+++ src/GestionClientes.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class GestionClientes {
+    private JLabel lblTitulo;
+    private JTextField txtId;
+    private JLabel lblId;
+    private JLabel lblNombre;
+    private JTextField txtNombre;
+    private JLabel lblRtn;
+    private JTextField txtRtn;
+    private JPanel panClientes;
+    private JButton btnGuardar;
+    private JPanel panBtn;
+    private JButton btnBuscar;
+    private JButton btnEliminar;
+    private JButton btnLimpiar;
+    private JButton btnModificar;
+    private JButton btnMenuP;
+    private JTable tblClientes;
+}
