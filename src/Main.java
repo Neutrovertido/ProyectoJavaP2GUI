8562200ae3a6bb85a69b199da6c6d94d608bc1b8
Neutrovertido <russellcode.james@gmail.com>
2023-06-28 15:19:07 -0600

    New way of instancing GUIs

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,26 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        GestionArticulos g = new GestionArticulos();
+        /*GestionArticulos g = new GestionArticulos();
         g.setTitle("Artículos");
         g.setContentPane(g.pnlArticulos);
         g.setSize(1000, 600);
         g.setVisible(true);
-        g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
+
+        JFrame ga = new JFrame("Artículos");
+        ga.setContentPane(new GestionArticulos().pnlArticulos);
+        ga.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        ga.pack();
+        ga.setResizable(false);
+        ga.setVisible(true);
+
+        JFrame gc = new JFrame("Clientes");
+        gc.setContentPane(new GestionClientes().pnlClientes);
+        gc.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        gc.setSize(600, 400);
+        gc.setResizable(false);
+        gc.setVisible(true);
 
         /*GestionClientes c = new GestionClientes();
         c.setTitle("Clientes");
