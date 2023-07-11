221d2edac6df5e395325407d101b317a46abf105
Neutrovertido <russellcode.james@gmail.com>
2023-07-11 15:55:02 -0600

    GestionFactura initial WIP

---
+++ src/GestionFactura.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class GestionFactura extends JFrame {
+    private JLabel lblTitulo;
+    private JLabel lblCodigo;
+    private JTextField txtCodigo;
+    private JLabel lblCliente;
+    private JTextField txtCliente;
+    private JSpinner spnCantidad;
+    private JLabel lblCantidad;
+    private JTextArea txaCliente;
+    private JTable tblFactura;
+    private JTextArea txaArticulo;
+    private Tienda t1;
+}
